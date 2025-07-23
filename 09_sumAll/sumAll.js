d9e9a2dc456cc8c3ab6f4a29c367d78dc403702d
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2025-07-23 09:55:28 +0100

    09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR"
+    if (start < 0 || end < 0) return "ERROR";
 
+    if (start > end) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    let total = 0;
+    for (let i = start; i <= end; i++) total += i;
+    return total;
 };
 
 // Do not edit below this line
